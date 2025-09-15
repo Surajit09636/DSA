1dd7f110b9192f806fc533be17d6a738898fd126
my-demo1 <162405009+Iconic89@users.noreply.github.com>
2025-09-15 19:57:36 +0530

    write SinglyLL code using util package in java

--- java/SinglyLL.java
+++ java/SinglyLL.java
@@ -1,46 +1,54 @@
-package java;
+import java.util.*;
 public class SinglyLL {
-    class Node{
-        int data;
-        Node next;
-        Node(int data){
-            this.data = data;
-            next = null;
-        }
-    }
-    Node head;
+    // class Node{
+    //     int data;
+    //     Node next;
+    //     Node(int data){
+    //         this.data = data;
+    //         next = null;
+    //     }
+    // }
+    // Node head;
 
-    public void insert(int data){
-        Node newNode = new Node(data);
-        if(head == null){
-            head = newNode;
-            return;
-        } else {
-            Node temp = head;
-            while(temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = newNode;
-        }
+    // public void insert(int data){
+    //     Node newNode = new Node(data);
+    //     if(head == null){
+    //         head = newNode;
+    //         return;
+    //     } else {
+    //         Node temp = head;
+    //         while(temp.next != null) {
+    //             temp = temp.next;
+    //         }
+    //         temp.next = newNode;
+    //     }
 
-    }
+    // }
 
-    void Display(){
-        Node temp = head;
-        while(temp != null){
-            System.out.print(temp.data + " -> ");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
+    // void Display(){
+    //     Node temp = head;
+    //     while(temp != null){
+    //         System.out.print(temp.data + " -> ");
+    //         temp = temp.next;
+    //     }
+    //     System.out.println("null");
+    // }
 
     public static void main(String args[]){
-        SinglyLL list = new SinglyLL();
-        list.insert(10);
-        list.insert(20);
-        list.insert(30);
-        list.insert(40);
-        list.insert(50);
-        list.Display();
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        System.out.println("Linked list" + list);
+        
+        // Treversing like a singly linked list
+        for(int data : list){
+            System.out.print(data + " -> ");
+        }
+        System.out.println("null");
     }
 }
