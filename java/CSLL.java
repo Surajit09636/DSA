0e2cd2183a2b5283b5abd18e4e4acc36a9bcf8e1
my-demo1 <162405009+Iconic89@users.noreply.github.com>
2025-09-19 09:23:19 +0530

    CSLL also done

---
+++ java/CSLL.java
@@ -0,0 +1,44 @@
+public class CSLL {
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+    Node head = null, tail = null;
+
+    public void insert(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+            tail.next = head;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+            tail.next = head;
+        }
+    }
+
+    public void Display(){
+        if(head == null){
+            return;
+        }
+        Node temp = head;
+        do{
+            System.out.print(temp.data + " ->");
+            temp = temp.next;
+        }while(temp != head);
+        System.out.println(head.data);
+    }
+
+    public static void main(String args[]){
+        CSLL list = new CSLL();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.Display();
+    }
+}
