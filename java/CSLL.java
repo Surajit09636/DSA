b9b50e3c673ff0e26d3a433bd64b18c8d6b099a1
my-demo1 <162405009+Iconic89@users.noreply.github.com>
2025-09-19 09:46:35 +0530

    CSLL using Util package

--- java/CSLL.java
+++ java/CSLL.java
@@ -1,38 +1,59 @@
+import java.util.*;
+
 public class CSLL {
-    class Node{
-        int data;
-        Node next;
-        Node(int data){
-            this.data = data;
-            next = null;
-        }
-    }
-    Node head = null, tail = null;
+    // class Node{
+    //     int data;
+    //     Node next;
+    //     Node(int data){
+    //         this.data = data;
+    //         next = null;
+    //     }
+    // }
+    // Node head = null, tail = null;
+
+    // public void insert(int data){
+    //     Node newNode = new Node(data);
+    //     if(head == null){
+    //         head = newNode;
+    //         tail = newNode;
+    //         tail.next = head;
+    //     } else {
+    //         tail.next = newNode;
+    //         tail = newNode;
+    //         tail.next = head;
+    //     }
+    // }
+
+    // public void Display(){
+    //     if(head == null){
+    //         return;
+    //     }
+    //     Node temp = head;
+    //     do{
+    //         System.out.print(temp.data + " ->");
+    //         temp = temp.next;
+    //     }while(temp != head);
+    //     System.out.println(head.data);
+    // }
+
+    LinkedList<Integer> list = new LinkedList<>();
 
     public void insert(int data){
-        Node newNode = new Node(data);
-        if(head == null){
-            head = newNode;
-            tail = newNode;
-            tail.next = head;
-        } else {
-            tail.next = newNode;
-            tail = newNode;
-            tail.next = head;
-        }
+        list.add(data);
     }
 
     public void Display(){
-        if(head == null){
+        if(list.isEmpty()){
             return;
         }
-        Node temp = head;
-        do{
-            System.out.print(temp.data + " ->");
-            temp = temp.next;
-        }while(temp != head);
-        System.out.println(head.data);
+        
+        for(int data : list){
+            System.out.print(data + " -> ");
+        }
+        System.out.println(list.getFirst());;
     }
+
+    
 
     public static void main(String args[]){
         CSLL list = new CSLL();
