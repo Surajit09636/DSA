4927bb2f18b63f9531c6dc89194b8fa7c94e03d1
my-demo1 <162405009+Iconic89@users.noreply.github.com>
2025-09-26 10:45:17 +0530

    Graph day 2

--- java/mygraph/graph.java
+++ java/mygraph/graph.java
@@ -6,10 +6,12 @@ public class graph {
     static class Edge {
         int src;
         int dest;
+        int wt;
 
-        public Edge(int s, int d) {
+        public Edge(int s, int d, int w) {
             this.src = s;
             this.dest = d;
+            this.wt = w;
         }
     }
 
@@ -18,17 +20,17 @@ public class graph {
             graph[i] = new ArrayList<>();
         }
 
-        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 2, 2));
 
-        graph[1].add(new Edge(1, 2));
-        graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 2, 10));
+        graph[1].add(new Edge(1, 3, 0));
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 1));
-        graph[2].add(new Edge(2, 3));
+        graph[2].add(new Edge(2, 0, 2));
+        graph[2].add(new Edge(2, 1, 10));
+        graph[2].add(new Edge(2, 3, -1));
 
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 2));
+        graph[3].add(new Edge(3, 1, 0));
+        graph[3].add(new Edge(3, 2, -1));
     }
 
     public static void main(String[] args) {
@@ -41,7 +43,7 @@ public class graph {
 
         for (int i = 0; i < graph[2].size(); i++) {
             Edge e = graph[2].get(i);
-            System.out.print(e.dest + " ");
+            System.out.println(e.dest + " , " + e.wt);
         }
     }
 }
